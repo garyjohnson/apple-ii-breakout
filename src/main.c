7e2bc1fcb49ff7e17491d89fb89eca754a1d9773
Gary Johnson <gary@gjtt.com>
2019-05-04 23:30:12 -0400

    Draw paddle

--- src/main.c
+++ src/main.c
@@ -2,58 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mouse.h>
-#include <conio.h>
-#include <ctype.h>
-#include <dbg.h>
-#include <cc65.h>
+#include <tgi.h>
 
-#define max(a,b)  (((a) > (b)) ? (a) : (b))
-#define min(a,b)  (((a) < (b)) ? (a) : (b))
+#define COLOR_BACK TGI_COLOR_BLACK
+#define COLOR_FORE TGI_COLOR_WHITE
+#define PADDLE_WIDTH 30
+#define PADDLE_HALF_WIDTH 15
+#define PADDLE_HEIGHT 2
 
-static void __fastcall__ CheckError(const char* action, unsigned char error)
-{
-  if (error != MOUSE_ERR_OK) {
-    cprintf("\n%s: %s(%u)\r\n", action, mouse_geterrormsg(error), error);
-
-    // Wait for keypress to show error before screen is cleared
-    if (doesclrscrafterexit()) {
-      cgetc();
-    }
-    exit(EXIT_FAILURE);
-  }
-}
+static unsigned maxY;
+static unsigned int paddleYTop, paddleYBottom;
+static unsigned int x = -1;
 
 int main (void)
 {
-    struct mouse_info info;
-    struct mouse_box full_box;
-    unsigned char width, height;
-    bool Invisible = true, Done = false, Jailed = false;
+  struct mouse_info info;
+  struct mouse_box full_box;
+  unsigned char error;
+  error = mouse_install(&mouse_def_callbacks, mouse_static_stddrv);
+  if(error != MOUSE_ERR_OK) {
+    exit(EXIT_FAILURE);
+  }
 
-    DbgInit(0);
+  tgi_install(tgi_static_stddrv);
+  if(tgi_geterror() != TGI_ERR_OK) {
+    exit(EXIT_FAILURE);
+  }
 
-    (void) bordercolor(COLOR_BLUE);
-    (void) bgcolor(COLOR_WHITE);
-    (void) textcolor(COLOR_BLACK);
-    cursor(0);
-    clrscr();
+  tgi_init();
+  if(tgi_geterror() != TGI_ERR_OK) {
+    exit(EXIT_FAILURE);
+  }
+  tgi_clear();
 
-    CheckError("mouse_install", mouse_install (&mouse_def_callbacks, mouse_static_stddrv));
+  maxY = tgi_getmaxy();
+  paddleYBottom = maxY - 10;
+  paddleYTop = paddleYBottom - PADDLE_HEIGHT;
 
-    /* Get the initial bounding box. */
-    mouse_getbox(&full_box);
+  mouse_getbox(&full_box);
 
-    screensize(&width, &height);
+  do {
+    mouse_info(&info);
 
-    clrscr();
+    if(info.pos.x == x) {
+      continue;
+    }
 
-    do {
-        mouse_info(&info);
-        gotoxy(0, 2);
-        cprintf(" X  = %3d\r\n", info.pos.x);
-        cprintf(" Y  = %3d\r\n", info.pos.y);
-    } while(!Done);
-
-    CheckError("mouse_uninstall", mouse_uninstall());
-    return EXIT_SUCCESS;
+    tgi_setcolor(COLOR_BLACK);
+    if(info.pos.x > x) {
+      tgi_bar(x-PADDLE_HALF_WIDTH,paddleYTop,info.pos.x-PADDLE_HALF_WIDTH,paddleYBottom);
+    } else if(info.pos.x < x) {
+      tgi_bar(info.pos.x+PADDLE_HALF_WIDTH,paddleYTop,x+PADDLE_HALF_WIDTH,paddleYBottom);
+    }
+    tgi_setcolor(COLOR_FORE);
+    tgi_bar(info.pos.x-PADDLE_HALF_WIDTH,paddleYTop,info.pos.x+PADDLE_HALF_WIDTH,paddleYBottom);
+    x=info.pos.x;
+  } while(true);
 }
